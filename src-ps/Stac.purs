68dd97a256d8b00225e1eeb51fa64325cebc91c1
James Santucci <james.santucci@gmail.com>
2021-02-11 17:19:33 -0700

    fix request for collections method

--- src-ps/Stac.purs
+++ src-ps/Stac.purs
@@ -1,22 +1,28 @@
 module Stac where
 
-import Affjax (Error, defaultRequest)
+import Affjax (Error(..), Response, defaultRequest)
 import Affjax as AX
 import Affjax.ResponseFormat as ResponseFormat
-import Data.Argonaut (JsonDecodeError, decodeJson)
-import Data.Either (Either)
-import Data.Typelevel.Undefined (undefined)
+import Data.Argonaut (Json, JsonDecodeError, decodeJson, printJsonDecodeError)
+import Data.Bifunctor (lmap)
+import Data.Either (Either(..))
 import Effect.Aff (Aff)
 import Model.Collection (StacCollection)
-import Prelude (bind, ($), (<$>), (<<<), (<>))
+import Prelude (bind, pure, ($), (<<<), (<>))
+
+adaptError :: JsonDecodeError -> Response Json -> Error
+adaptError jsErr resp =
+  RequestContentError
+    ( "Request failed to produce a meaningful response: " <> printJsonDecodeError jsErr
+    )
 
 getCollections :: String -> Aff (Either Error (Array StacCollection))
 getCollections apiHost = do
   resp <-
     AX.request
       $ defaultRequest { url = apiHost <> "/collections", responseFormat = ResponseFormat.json }
-  let
-    result = (decodeCollection <<< _.body) <$> resp
-  undefined
+  case resp of
+    Left e -> pure $ Left e
+    Right success -> pure $ lmap (\err -> adaptError err success) $ (decodeCollection <<< _.body) success
   where
   decodeCollection js = decodeJson js :: Either JsonDecodeError (Array StacCollection)
